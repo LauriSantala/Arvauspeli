f59f176de8b829862a0e0eb53545ce07cc5820ea
LauriSantala <lauri23008@student.hamk.fi>
2023-09-04 09:48:04 +0300

    Initial commit

---
+++ src/Testaus.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Testaus {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		String vastaus = "Lauri";
+		String input = "";
+		int arvauksia = 0;
+		
+		
+		System.out.println("Arvaa nimeni (kirjoita 'loppu' lopettaaksesi)");
+		
+		
+		while (!input.equalsIgnoreCase("loppu")) {
+		input = in.nextLine();	
+		
+		if (input.equals("loppu"))
+			break;
+		arvauksia++;
+		if (input.equalsIgnoreCase(vastaus)) {
+			System.out.println ("Onneksiolkoon!");
+			break;
+		}
+		else {
+			System.out.println("Arvaa nimeni (kirjoita 'loppu' lopettaaksesi)");
+		}
+		
+		}
+		System.out.println ("Arvasit " + arvauksia + " kertaa. ");
+		
+	
+	}
+	
+}
